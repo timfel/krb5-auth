b02ec9b5980f2c930a4054653dc498ddf2ed2ee1
clalance <clalance@44487d05-f7dd-44fb-8a1c-a29a52002039>
2008-05-19 12:24:06 +0000

    Add a couple more lines of pretty printing to the example.rb.

--- bin/example.rb
+++ bin/example.rb
@@ -6,7 +6,7 @@ krb5 = Krb5.new
 
 # get the default realm
 default_realm = krb5.get_default_realm
-puts default_realm
+puts "Default realm is: " + default_realm
 
 # try to cache non-existant data (this should fail and throw an exception)
 begin
@@ -20,6 +20,8 @@ krb5.get_init_creds_keytab
 
 # cache those credentials in the default cache location
 krb5.cache
+
+puts "Principal: " + krb5.get_default_principal
 
 # destroy those same credentials from the default cache location
 krb5.destroy
