e91ca73365d8b055425863794b32f0398b2eb853
Mike Limansky <mike.limansky@gmail.com>
2014-12-24 01:28:19 +0300

    Add libcom_err to linking dependencies.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,4 +5,5 @@ extension_name = 'krb5_auth'
 dir_config(extension_name)
 have_library("c", "main")
 have_library("krb5","krb5_init_context")
+have_library("com_err","error_message")
 create_makefile('krb5_auth')
