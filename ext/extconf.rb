703a766cb54dac8a332ef3e03e626a7991dea166
Tim Felgentreff <timfelgentreff@gmail.com>
2015-01-13 21:48:58 +0100

    raise errors if krb5 lib or headers cannot be found

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,8 @@ RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 extension_name = 'krb5_auth'
 dir_config(extension_name)
 have_library("c", "main")
-have_library("krb5","krb5_init_context")
+raise "Cannot find krb5 library" unless have_library("krb5","krb5_init_context")
+raise "Cannot find krb5 headers" unless have_header("krb5.h")
 have_library("com_err","error_message")
 have_header("com_err.h")
 create_makefile('krb5_auth')
